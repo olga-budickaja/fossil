bf4c49c00973693c3cc7501ebecedd706010c394
Olga <boa906680@gmail.com>
2022-09-28 23:50:59 +0300

    add Tabs

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,28 @@ hamburger.addEventListener('click', () => {
     menuMobile.classList.toggle("header__wrapper_active");
     hamburger.classList.toggle("close");
 });
+
+//TABS
+const tabs = document.querySelectorAll(".collection__tab");
+const contentsTab = document.querySelectorAll(".collection__content");
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        let tabId = tab.getAttribute("data-tab");
+        let contentTab = document.querySelector(tabId);
+
+        tabs.forEach(tab => {
+            tab.classList.remove("collection__tab_active");
+        });
+
+        contentsTab.forEach(content => {
+            content.classList.remove("collection__content_active");
+        });
+
+        tab.classList.add("collection__tab_active");
+        contentTab.classList.add("collection__content_active");
+    });
+});
+
+
+
